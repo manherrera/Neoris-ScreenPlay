5fdc7c319c3a25a16cb20c88e5d70f045ddee6a9
aledepetris <aledepetris@outlook.com>
2020-02-07 14:52:55 -0300

    Paso 18 - Se implementa el metodo con la anotación @Given.

--- src/test/java/com/neoris/screenplay/stepdefinition/InicioSesionStepDefinition.java
+++ src/test/java/com/neoris/screenplay/stepdefinition/InicioSesionStepDefinition.java
@@ -5,10 +5,15 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+
 public class InicioSesionStepDefinition {
+
+    private InicioSesionPage inicioSesionPage;
 
     @Before
     public void setTheStage(){
@@ -16,9 +21,10 @@ public class InicioSesionStepDefinition {
     }
 
     @Given("^(.*) quiere autenticarse$")
-    public void productorQuiereAutenticarse() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void productorQuiereAutenticarse(String actor) {
+        theActorCalled(actor).attemptsTo(
+                Open.browserOn(inicioSesionPage)
+        );
     }
 
     @When("^Productor se autentica$")
