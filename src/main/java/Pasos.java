8fd31e73df6e187471536ce32d2c954207564276
aledepetris <aledepetris@outlook.com>
2020-02-07 13:03:02 -0300

    Se actualizan las indicaciones en los archivos "Pasos". El proyecto esta listo para configurar las dependencias.

--- src/main/java/Pasos.java
+++ src/main/java/Pasos.java
@@ -18,12 +18,12 @@ public class Pasos {
 
     // TODO 33 Crear UsuarioBuilder.java dentro del paquete model
 
-    // TODO 34 Validar el estado del modelo usuario verificando que los string no estan vacios o nulos
+    // TODO 34 Validar el estado del modelo usuario verificando que los string no estan vacíos o nulos
     // TODO 35 Crear el paquetes exception dentro del paquete base
     // TODO 36 Crear la excepción UsuarioEstadoIlegalException.java dentro del paquete exception
 
     // TODO 37 Crear el paquetes util dentro del paquete base
-    // TODO 38 Crear la clase ValidadorString.java y mover el método que
+    // TODO 38 Crear la clase ValidadorString.java y mover el método que valida que los string no estan vacíos o nulos
 
     // TODO 40 Crear el paquetes question dentro del paquete base
     // TODO 41 Crear la question
@@ -36,6 +36,8 @@ public class Pasos {
 
     // TODO 49 Configurar las variables -Dusuario= y -Dcontrasena= para no tener esos datos en el código
     // TODO 50 Configurar las variabes -Dcontext= -Dwebdriver.driver= para configurar el contexto y el driver a usar
+    //  Crear método que devuelva un usuario por defecto
+    //  El usuario por defecto debe obtener los valores de sus atributos de las variables -Dusuario= y -Dcontrasena=
 
     // TODO 53 Crear la excepción AutenticacionFallidaError.java dentro del paquete exception
 }
