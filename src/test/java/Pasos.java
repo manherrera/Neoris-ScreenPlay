e0669b91621e5fbccd5062f02d64a87d34a6b463
aledepetris <aledepetris@outlook.com>
2020-02-07 14:23:18 -0300

    Pasos 11,12 y 13 - Se crea la Clase InicioSesion (Runner)

--- src/test/java/Pasos.java
+++ src/test/java/Pasos.java
@@ -5,7 +5,7 @@ public class Pasos {
     /*
       @RunWith(CucumberWithSerenity.class)
       @CucumberOptions(features = "src/test/resources/features/inicio_sesion.feature",
-      glue = {"com.cedaniel200.screenplay.dojo.stepdefinition"}, snippets = SnippetType.CAMELCASE)
+      glue = {"com.neoris.screenplay.stepdefinition"}, snippets = SnippetType.CAMELCASE)
      */
 
 
---
+++ src/test/java/com/neoris/screenplay/runner/InicioSesion.java
@@ -0,0 +1,14 @@
+package com.neoris.screenplay.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+    @RunWith(CucumberWithSerenity.class)
+    @CucumberOptions(features = "src/test/resources/features/inicio_sesion.feature",
+            glue = {"com.neoris.screenplay.stepdefinition"}, snippets = SnippetType.CAMELCASE)
+
+    public class InicioSesion {
+
+    }
