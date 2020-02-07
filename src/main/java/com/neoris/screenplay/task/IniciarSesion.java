aebd82c8c539d1d6e6731d86818c9cf084383915
aledepetris <aledepetris@outlook.com>
2020-02-07 15:38:34 -0300

    Pasos 28 a 30 - Implementacion de la tarea IniciarSesion y Mapeados de los elementos Web con la clase de UserInterface

--- src/main/java/com/neoris/screenplay/task/IniciarSesion.java
+++ src/main/java/com/neoris/screenplay/task/IniciarSesion.java
@@ -3,15 +3,29 @@ package com.neoris.screenplay.task;
 import com.neoris.screenplay.model.Usuario;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+
+import static com.neoris.screenplay.userinterface.InicioSesionPage.*;
+import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class IniciarSesion implements Task {
 
-    @Override
-    public <T extends Actor> void performAs(T actor) {
+    private Usuario usuario;
 
+    public IniciarSesion(Usuario usuario) {
+        this.usuario = usuario;
     }
 
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Enter.theValue(usuario.getUsuario()).into(NOMBRE_USUARIO_O_CORREO),
+                Enter.theValue(usuario.getContrasena()).into(CONTRASENA),
+                Click.on(INGRESAR)
+        );
+    }
     public static IniciarSesion conElUsuario(Usuario usuario) {
-        return null;
+        return instrumented(IniciarSesion.class, usuario);
     }
 }
--- src/main/java/com/neoris/screenplay/userinterface/InicioSesionPage.java
+++ src/main/java/com/neoris/screenplay/userinterface/InicioSesionPage.java
@@ -1,9 +1,16 @@
 package com.neoris.screenplay.userinterface;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.annotations.DefaultUrl;
 
 @DefaultUrl("https://sapd-wdd.cooperacionseguros.com.ar:44300/sap/bc/ui2/flp")
 public class InicioSesionPage extends PageObject {
+
+    public static final Target NOMBRE_USUARIO_O_CORREO = Target.the("Username or email field")
+            .located(By.id("USERNAME_FIELD-inner"));
+    public static final Target CONTRASENA = Target.the("Password field").located(By.id("PASSWORD_FIELD-inner"));
+    public static final Target INGRESAR = Target.the("Sign in").located(By.id("LOGIN_LINK"));
 
 }
