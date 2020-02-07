e0591bc83410b5f66347f5dd3220617737fc1a68
aledepetris <aledepetris@outlook.com>
2020-02-07 15:12:10 -0300

    Pasos 24 al 26 - Creacion de la Clase de Usuario y paqeutess "task" y "model"

---
+++ src/main/java/com/neoris/screenplay/model/Usuario.java
@@ -0,0 +1,20 @@
+package com.neoris.screenplay.model;
+
+public class Usuario {
+
+    private final String usuario;
+    private final String contrasena;
+
+    public Usuario(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+}
